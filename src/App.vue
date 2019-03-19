d4c1efb945e4382bad5986e4287405eddb55c31f
Adam Houston <phazyy9@gmail.com>
2019-03-19 04:36:26 +0000

    Making things semi-pretty

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
+    <v-toolbar color="teal" dark prominent extended>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>
         <span>DEPLOY</span>
         <span class="font-weight-light">BUNDLES</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
     </v-toolbar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-layout row pb-2>
+      <v-flex xs8 offset-xs2>
+        <v-card class="card--flex-toolbar">
+          <HelloWorld/>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-app>
 </template>
 
@@ -29,3 +38,9 @@ export default {
   }
 }
 </script>
+
+<style>
+  .card--flex-toolbar {
+    margin-top: -64px;
+  }
+</style>
