4bc0207e09a0911ee9b67823c7bfb4b80eab35e1
Adam Houston <phazyy9@gmail.com>
2019-03-19 04:56:27 +0000

    Splitting navigation drawer into it's own component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      clipped
-      class="grey lighten-4"
-      app
-    >
-      <v-list
-        dense
-        class="grey lighten-4"
-      >
-        <template v-for="(item, i) in items">
-          <v-layout
-            v-if="item.heading"
-            :key="i"
-            row
-            align-center
-          >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-          </v-layout>
-          <v-divider v-else-if="item.divider" :key="i" dark class="my-3" />
-          <v-list-tile v-else :key="i">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title class="grey--text">
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-
+    <NavigationDrawer />
     <v-toolbar color="teal" dark prominent extended>
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title>
@@ -64,24 +26,15 @@
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import NavigationDrawer from './components/NavigationDrawer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavigationDrawer
   },
-  data: () => ({
-    items: [
-        { heading: 'Cluster' },
-        { icon: 'add', text: 'Deployments' },
-        { icon: 'touch_app', text: 'Builder' },
-        { icon: 'help', text: 'Visualiser' },
-        { divider: true },
-        { icon: 'settings', text: 'Settings' },
-        { icon: 'help', text: 'Help' },
-        { icon: 'phonelink', text: 'App downloads' }
-      ]
-  })
+  data: () => ({ })
 }
 </script>
 
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      class="grey lighten-4"
+      app
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-layout
+            v-if="item.heading"
+            :key="i"
+            row
+            align-center
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+          </v-layout>
+          <v-divider v-else-if="item.divider" :key="i" dark class="my-3" />
+          <v-list-tile v-else :key="i">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+        { heading: 'Cluster' },
+        { icon: 'add', text: 'Deployments' },
+        { icon: 'touch_app', text: 'Builder' },
+        { icon: 'help', text: 'Visualiser' },
+        { divider: true },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' }
+      ]
+  })
+}
+</script>
