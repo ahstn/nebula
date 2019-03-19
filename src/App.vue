0cd23575e49ac839b5de5b937a68cf9f11e0077c
Adam Houston <phazyy9@gmail.com>
2019-03-19 04:53:44 +0000

    Adding Navigation Drawer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,44 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      class="grey lighten-4"
+      app
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-layout
+            v-if="item.heading"
+            :key="i"
+            row
+            align-center
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+          </v-layout>
+          <v-divider v-else-if="item.divider" :key="i" dark class="my-3" />
+          <v-list-tile v-else :key="i">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-toolbar color="teal" dark prominent extended>
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title>
@@ -13,8 +52,8 @@
       </v-btn>
     </v-toolbar>
 
-    <v-layout row pb-2>
-      <v-flex xs8 offset-xs2>
+    <v-layout row pb-2 class="grey lighten-4">
+      <v-flex xs9 offset-xs2>
         <v-card class="card--flex-toolbar">
           <HelloWorld/>
         </v-card>
@@ -31,16 +70,30 @@ export default {
   components: {
     HelloWorld
   },
-  data () {
-    return {
-      //
-    }
-  }
+  data: () => ({
+    items: [
+        { heading: 'Cluster' },
+        { icon: 'add', text: 'Deployments' },
+        { icon: 'touch_app', text: 'Builder' },
+        { icon: 'help', text: 'Visualiser' },
+        { divider: true },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' }
+      ]
+  })
 }
 </script>
 
-<style>
-  .card--flex-toolbar {
-    margin-top: -64px;
-  }
+<style lang="stylus">
+  .card--flex-toolbar
+    margin-top: -64px
+
+  .v-navigation-drawer
+    margin-top: 128px!important
+    width: 220px!important
+    z-index: 0
+
+  .v-navigation-drawer__border
+    display: none
 </style>
